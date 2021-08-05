52dd1a53fce09d8a0ff87088b32f4b5be66c8781
cbtkhr <cbtkhr125@gmail.com>
2021-08-05 14:33:57 +0900

    feat: Update main.ts

--- dist/index.js
+++ dist/index.js
@@ -2272,17 +2272,15 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const token = core.getInput('token', { required: true });
-            const participationIds = core
-                .getInput('participationId')
-                .split(',')
-                .map(id => id.trim());
-            const groupIds = core
-                .getInput('groupId')
-                .split(',')
-                .map(id => id.trim());
-            if (!participationIds.length && !groupIds.length) {
+            const participationIdInput = core.getInput('participationId');
+            const groupIdInput = core.getInput('groupId');
+            if (!participationIdInput && !groupIdInput) {
                 throw new Error(`participationId or groupId must be set`);
             }
+            const participationIds = participationIdInput
+                .split(',')
+                .map(id => id.trim());
+            const groupIds = groupIdInput.split(',').map(id => id.trim());
             const content = core.getInput('content', { required: true });
             const requests = participationIds.map(participationId => got_1.default.post(`https://www.sonicgarden.world/room_api/v1/rooms/participations/${participationId}/comments?token=${token}`, {
                 json: {
--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,17 @@ import got from 'got'
 async function run(): Promise<void> {
   try {
     const token = core.getInput('token', {required: true})
-    const participationIds = core
-      .getInput('participationId')
-      .split(',')
-      .map(id => id.trim())
-    const groupIds = core
-      .getInput('groupId')
-      .split(',')
-      .map(id => id.trim())
-    if (!participationIds.length && !groupIds.length) {
+
+    const participationIdInput = core.getInput('participationId')
+    const groupIdInput = core.getInput('groupId')
+    if (!participationIdInput && !groupIdInput) {
       throw new Error(`participationId or groupId must be set`)
     }
+    const participationIds = participationIdInput
+      .split(',')
+      .map(id => id.trim())
+    const groupIds = groupIdInput.split(',').map(id => id.trim())
+
     const content = core.getInput('content', {required: true})
 
     const requests = participationIds.map(participationId =>
